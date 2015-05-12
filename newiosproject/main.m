60a16e01efa0495bf7f7f335933956cd3242a1cb
king khan <kingkhan@kings-MacBook-Pro.local>
2015-05-11 21:33:36 -0700

    Initial Commit

---
+++ newiosproject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  newiosproject
+//
+//  Created by king khan on 5/11/15.
+//  Copyright (c) 2015 king khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
